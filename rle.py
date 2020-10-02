a36a28a123aa9d9dce9b02451890d2340400284f
Jacob Vad <jacobvad1010rgmail.com>
2020-10-02 11:47:49 +0200

    fejl i

--- rle.py
+++ rle.py
@@ -2,11 +2,11 @@
 def rle(st):
     j = 0
     res =[]
-    while j < len(st):
+    while j < lensss(st):
         i = 0
         ch = st[j]
         while j < len(st) and st[j]==ch:
             i += 1
             j+=1
-        res.append(f'{i}{ch}')
+                    res.append(f'{i}{ch}')
     return ''.join(res)
